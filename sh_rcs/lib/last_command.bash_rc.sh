be2273dcf1f451c7c4e9b71f8bfa8a3d5e1ff073
Kuba <kkatarzynski@gmail.com>
2015-03-03 11:12:36 +0000

    Update last_command.bash_rc.sh

--- sh_rcs/lib/last_command.bash_rc.sh
+++ sh_rcs/lib/last_command.bash_rc.sh
@@ -1,4 +1,5 @@
-# Simple solution based on http://stackoverflow.com/a/1862762 
+# Simple solution based on http://stackoverflow.com/a/1862762
+# by http://stackoverflow.com/users/7446/ville-laurikari
 trap __timer_start DEBUG
 function __timer_start() {
 	__timer=${__timer:-$SECONDS}
